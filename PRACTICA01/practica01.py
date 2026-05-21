417a60cc67e7b13f3aa106025e6f248fa2e398b8
labravo20 <labravo@unal.edu.co>
2026-05-21 18:11:31 -0500

    comentario

--- PRACTICA01/practica01.py
+++ PRACTICA01/practica01.py
@@ -1,9 +1,11 @@
 
 # Generando 'print' identificador del entorno de programación
-print("Inicializando entorno de programación --> Práctica 01 OPTOELECTRÓNICA")
+print("Inicializando entorno de programación --> Interfaz Práctica 01 OPTOELECTRÓNICA")
 
 
 import numpy as np
 import matplotlib.pyplot as plt
+
+
 
 
