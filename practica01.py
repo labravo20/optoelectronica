c0436b7457e170fb4dd05bf73098670e038b81d8
labravo20 <labravo@unal.edu.co>
2026-05-21 17:54:57 -0500

    Creación de documento base para implementación PRÁCTICA 01 OPTOELECTRÓNICA

---
+++ practica01.py
@@ -0,0 +1,9 @@
+
+# Generando 'print' identificador del entorno de programación
+print("Inicializando entorno de programación --> Práctica 01 OPTOELECTRÓNICA")
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+
