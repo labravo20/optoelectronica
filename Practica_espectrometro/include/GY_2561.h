dfdcae293212c589de8c009e94bc25c7f9be0283
mparejav <mparejav@unal.edu.co>
2026-05-21 19:33:37 -0500

    GY2561 toma datos

---
+++ Practica_espectrometro/include/GY_2561.h
@@ -0,0 +1,12 @@
+#ifndef GY_2561_H
+#define GY_2561_H
+
+#include <Wire.h>
+#include <Arduino.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_TSL2561_U.h>
+
+extern float lux;
+#define GY_2561_ADDR 0x39
+
+#endif // GY_2561_H
--- Practica_espectrometro/src/main.cpp
+++ Practica_espectrometro/src/main.cpp
@@ -1,5 +1,15 @@
 #include <Arduino.h>
 #include "28BYJ_48.h"
+#include "GY_2561.h"
+#include <Wire.h>
+
+/* I2C comm*/
+#define I2C_SDA1 8
+#define I2C_SCL1 9
+
+TwoWire I2C_ONE = TwoWire(0);
+
+Adafruit_TSL2561_Unified tsl = Adafruit_TSL2561_Unified(TSL2561_ADDR_FLOAT, GY_2561_ADDR);
 
 void setup() {
 
@@ -12,11 +22,22 @@ void setup() {
   pinMode(motorPin3, OUTPUT);
   pinMode(motorPin4, OUTPUT);
 
+  I2C_ONE.begin(I2C_SDA1, I2C_SCL1, 50000);  // I2C Bus 1
+
+  delay(10);
+
+  if(!tsl.begin(&I2C_ONE))
+  {
+    /* There was a problem detecting the TSL2561 ... check your connections */
+    Serial.print("Ooops, no TSL2561 detected ... Check your wiring or I2C ADDR!");
+    while(1);
+  }
+
 }
 
 void loop() {
 
-  for (int i = 0; i < stepsPerRev; i++)
+  /*for (int i = 0; i < stepsPerRev; i++)
   {
     clockwise();
     delayMicroseconds(motorSpeed);
@@ -33,5 +54,28 @@ void loop() {
     Serial.print("Counter clockwise step: ");
     Serial.println(i + 1);    
   }
-  delay(1000);
+  delay(1000);*/
+
+
+  /* Get a new sensor event */ 
+  sensors_event_t event;
+  tsl.getEvent(&event);
+ 
+  /* Display the results (light is measured in lux) */
+  if (event.light)
+  {
+    Serial.println(event.light);
+  }
+  else
+  {
+    /* If event.light = 0 lux the sensor is probably saturated
+       and no reliable data could be generated! */
+    Serial.println("Sensor overload");
+  }
+  delay(250);
 }
+
+
+
+
+
